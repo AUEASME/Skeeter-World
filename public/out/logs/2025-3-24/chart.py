47ecdf187e31a0f0a15078c433c371925a2f7e04
James <jlb0181@auburn.edu>
2025-08-13 15:03:26 -0500

    Add old logs

---
+++ public/out/logs/2025-3-24/chart.py
@@ -0,0 +1,50 @@
+import matplotlib.pyplot as plt
+
+# Find all files named "experiment_*.json" in the current directory.
+import glob
+import json
+
+# Get the list of all files in the current directory.
+files = glob.glob("experiment_*.json")
+
+# Read the data from all files.
+data = []
+for file in files:
+    with open(file) as f:
+        data.append(json.load(f))
+
+# Create a 2D scatter plot charting killRate vs. rescueRate vs. max(infectionRatio), with the infectionRatio determining the size of the points.
+fig = plt.figure()
+
+x = []
+y = []
+s = []
+colors = []
+max_size = 0
+for experiment in data:
+    if (max(experiment["infectionRatio"]) < 0.125):
+        continue
+    x.append(experiment["killRate"])
+    y.append(experiment["rescueRate"])
+    s.append(max(experiment["infectionRatio"]))
+    max_size = max(max_size, max(experiment["infectionRatio"]))
+
+for experiment in data:
+    if (max(experiment["infectionRatio"]) < 0.125):
+        continue
+    if max(experiment["infectionRatio"]) > (max_size / 2):
+        colors.append("red")
+    elif max(experiment["infectionRatio"]) > (max_size / 4):
+        colors.append("orange")
+    elif max(experiment["infectionRatio"]) > (max_size / 8):
+        colors.append("gold")
+    else:
+        colors.append("limegreen")
+
+plt.scatter(x, y, s=[1000 * (size / max_size) for size in s], alpha=.5, c=colors)
+
+plt.xlabel("Kill Rate")
+plt.ylabel("Rescue Rate")
+plt.title("Kill Rate vs. Rescue Rate vs. Max Infection Ratio")
+
+plt.show()
